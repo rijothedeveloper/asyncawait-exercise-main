e89ddc0c7779db3456eceee4028329963fe9da0d
Rijo George <rijothedeveloper@gmail.com>
2022-01-17 15:10:49 -0800

    done

--- deck_of_cards/script.js
+++ deck_of_cards/script.js
@@ -1,48 +1,43 @@
 class Card {
     cardId = 0;
     rotation = 0;
-    constructor() {
-        axios.get("http://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
-        .then(response => {
-            this.cardId = response.data.deck_id
+
+    async init() {
+        const response = await axios.get("http://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
+        this.cardId = response.data.deck_id;
             console.log(this.cardId);
-            this.getNewCard()
-        })                                                             
+            this.getNewCard();                                                          
     }
 
-    getNewCard() {
-        axios.get(`http://deckofcardsapi.com/api/deck/${this.cardId}/draw/?count=1`)
-            .then(response => {
-                const card = response.data.cards[0]
-                console.log(card.value)
-                console.log(card.suit)
-                return axios.get(`http://deckofcardsapi.com/api/deck/${this.cardId}/draw/?count=1`)
-            })
-            .then(response => {
-                const card = response.data.cards[0]
-                console.log(card.value)
-                console.log(card.suit)
-            })
+    async getNewCard() {
+        let response = await axios.get(`http://deckofcardsapi.com/api/deck/${this.cardId}/draw/?count=1`)
+        const card1 = response.data.cards[0]
+        console.log(card1.value)
+        console.log(card1.suit)
+
+        response = await axios.get(`http://deckofcardsapi.com/api/deck/${this.cardId}/draw/?count=1`)
+        const card2 = response.data.cards[0]
+        console.log(card2.value)
+        console.log(card2.suit)
     }
 
-    appendCardImage() {
-        axios.get(`http://deckofcardsapi.com/api/deck/${this.cardId}/draw/?count=1`)
-            .then(response => {
-                const card = response.data.cards[0]
-                const newImg = document.createElement("img");
-                newImg.style.position = "absolute";
-                newImg.style.top = "60px"
-                newImg.style.left = "100px"
-                newImg.style.transform = `rotate(${this.rotation}deg)`;
-                this.rotation += 30;
-                newImg.setAttribute("src", card.image)
-                document.getElementById("img-section").appendChild(newImg)
-            })
+    async appendCardImage() {
+        const response = await axios.get(`http://deckofcardsapi.com/api/deck/${this.cardId}/draw/?count=1`)
+        const card = response.data.cards[0]
+        const newImg = document.createElement("img");
+        newImg.style.position = "absolute";
+        newImg.style.top = "60px"
+        newImg.style.left = "100px"
+        newImg.style.transform = `rotate(${this.rotation}deg)`;
+        this.rotation += 30;
+        newImg.setAttribute("src", card.image)
+        document.getElementById("img-section").appendChild(newImg)
     }
 
 }
 
 const card = new Card();
+card.init()
 const newButton = document.getElementById("new-card");
 newButton.addEventListener("click", event => {
     card.appendCardImage()
